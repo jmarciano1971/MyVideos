337d13ed1da4ed4eab1c58bc6f192325a3967c34
Jairo M Silva <jmarciano1971@gmail.com>
2017-10-29 00:01:32 -0200

    ok

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ body{width:615px;font-family:arial;letter-spacing:1px;line-height:20px;}
 	if(!empty($_POST['search']['keyword'])) {
 		$search_keyword = $_POST['search']['keyword'];
 	}
-	$sql = 'select idMovie, idFile, c16 , c02, c16, c19, c22, c00, c20 from movie WHERE c00 LIKE :keyword OR c16 LIKE :keyword order by premiered desc ';
+	$sql = 'select idMovie, idFile, c16 , c02, c16, c19, c22, c00, c08 from movie WHERE c00 LIKE :keyword OR c16 LIKE :keyword order by premiered desc ';
 
 	/* Pagination Code starts */
 	$per_page_html = '';
