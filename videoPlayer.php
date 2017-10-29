2f6be5a320891c950bc30e2250260573c1ded476
Jairo M Silva <jmarciano1971@gmail.com>
2017-10-29 03:29:06 -0200

    ok

--- videoPlayer.php
+++ videoPlayer.php
@@ -11,7 +11,7 @@
 
   	if ($path_parts['extension'] != 'mp4') echo "<br><h3>*** Este vídeo não tem a extensão .mp4, portanto só possui a garantia de total funcionamento no Google Chrome. E mesmo assim, poderá ter problemas de lentidão para avançar e retroceder ...</h3><br>";
 
-    echo "<h4>" . $_GET["title"] . "</h4>;
+    echo "<h4>" . $_GET["title"] . "</h4>";
 
     if (file_exists($path_parts['dirname'] . "/" . $path_parts['filename'] . "..srt"))
 		   $filename_srt = $path_parts['dirname'] . "/" . $path_parts['filename'] . "..srt";
