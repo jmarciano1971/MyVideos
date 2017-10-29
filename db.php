8e78b264d1994af4d2481a036658283114fd7f20
Jairo M Silva <jmarciano1971@gmail.com>
2017-10-28 23:02:57 -0200

    ok

---
+++ db.php
@@ -0,0 +1,5 @@
+<?php
+	$database_username = 'root';
+	$database_password = 'sophia2003';
+	$pdo_conn = new PDO( 'mysql:host=localhost;dbname=MyVideos107', $database_username, $database_password );
+?>
