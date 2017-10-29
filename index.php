ca9e766bfbe7202bd5cb4f3e8a272a65c1d9d3ec
Jairo M Silva <jmarciano1971@gmail.com>
2017-10-29 01:01:49 -0200

    ok

--- index.php
+++ index.php
@@ -96,7 +96,10 @@ body{width:615px;font-family:arial;letter-spacing:1px;line-height:20px;}
 
 	?>
 	  <tr class='table-row'>
-		<td><a href=videoPlayer.php?filename=""<?php echo $filename;?>""&poster=""<?php echo $img; ?>""><img src=<?php echo $img; ?> alt="Smiley face" height="160" width="120"></a></td>
+		<td><a
+    <?php echo "<a href=\"videoPlayer.php?filename=$filename\">";?>
+			<img src=<?php echo $img; ?> alt="Smiley face" height="160" width="120">
+		</a></td>
 		<td><?php echo $row['c16']; ?></td>
 		<td><?php echo utf8_encode($row['c00']); ?></td>
 		<td><?php echo $row['premiered']; ?></td>
